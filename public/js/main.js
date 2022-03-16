c270a8d4253d5dd1774c04d14847147729844b8a
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2022-03-16 13:15:51 +0100

    remove the algo button

--- public/js/main.js
+++ public/js/main.js
@@ -20,63 +20,6 @@ async function loadFilters(filters) {
     addTag(filter, getTagColor(getTagList(recipeList), filter))
   );
 }
-document.getElementById('stats-button').addEventListener('click', () => {
-  changeAlgo()
-}) 
-function changeAlgo() {
-  function getAlgorithm() {
-    switch (sessionStorage.getItem("algo")) {
-      case "2":
-        sessionStorage.setItem("algo", "1");
-        break;
-      case "1":
-        sessionStorage.setItem("algo", "2");
-        break;
-      case undefined:
-        sessionStorage.setItem("algo", "1");
-        break;
-      case null:
-        sessionStorage.setItem("algo", "1");
-        break;
-    }
-  }
-  getAlgorithm();
-
-  // coco recipelist
-  let time = searchRecipe(recipeList, recipeList, true);
-  let number = document.getElementById("recipes-wrapper").childElementCount;
-
-  console.group((label = "Algorithme changé:"));
-  console.info(
-    `%cAlgorithme n°${sessionStorage.getItem("algo")}`,
-    "font-weight: bold; color: yellow;"
-  );
-  switch (true) {
-    case 0 <= time && time <= 15:
-      console.info(
-        `Temps d'execution: %c${time}ms`,
-        "font-weight: bold; color: lime;"
-      );
-      break;
-    case 15 < time && time < 40:
-      console.info(
-        `Temps d'execution: %c${time}ms`,
-        "font-weight: bold; color: orange;"
-      );
-      break;
-    case time >= 40:
-      console.info(
-        `Temps d'execution: %c${time}ms`,
-        "font-weight: bold; color: red;"
-      );
-      break;
-  }
-  console.info(
-    `Nombre d'objets traîtés: %c${number}`,
-    "font-weight: bold; color: white;"
-  );
-  console.groupEnd();
-}
 function getTime(txt, item) {
   let time;
   switch (item) {
@@ -129,9 +72,6 @@ function getTagColor(list, tag) {
 }
 function addTag(tag, color) {
   
-  /*if(getStatut().filter(el => el == tag)) {
-    console.log(`tag ${tag} already exist`)
-  }*/
     let wrapper = document.getElementById("tags-wrapper");
     let tagDOM = document.createElement("div");
   
